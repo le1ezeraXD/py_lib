5811a05ab66e200868cb3e164d28d96f78e26aa0
Dorgon <dorgon.aisin@gmail.com>
2025-02-18 07:55:37 +0800

    复制csv200次 并处理每一次的testParameters

--- csv_repeat.py
+++ csv_repeat.py
@@ -1,10 +1,56 @@
-import pandas as pd
+import csv
 
-#repeat line 2 in csv
-df = pd.read_csv('/Users/device/Desktop/test.csv')
 
-df_expanded = df.loc[df.index.repeat(2)].reset_index(drop=True)
+# 定义一个函数来返回带有序号后缀的字符串
+# def get_ordinal_suffix(n):
+#     if 10 <= n % 100 <= 20:
+#         suffix = 'th'
+#     else:
+#         suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(n % 10, 'th')
+#     return f'{n}{suffix}'
 
-df_expanded.to_csv('/Users/device/Desktop/test_expanded.csv', index=False)
 
-print("over")
+# 统一的 test Parameters 参数前缀
+unified_test_param = "Unified Test Parameter"
+limit_set = set()
+main_set = set()
+
+# 读取原始CSV文件
+with open('limit_test.csv', mode='r', newline='', encoding='utf-8') as infile:
+    reader = csv.DictReader(infile)
+    rows = list(reader)  # 将原始CSV的内容读取到列表中
+    for row in rows:
+        sb = row["Coverage"] + row["TestParameters"]
+        limit_set.add(sb)
+
+with open('main_test.csv', mode='r', newline='', encoding='utf-8') as infile:
+    reader = csv.DictReader(infile)
+    rows = list(reader)  # 将原始CSV的内容读取到列表中
+    for row in rows:
+        sb = row["Coverage"] + row["TestParameters"]
+        main_set.add(sb)
+
+
+for sb in limit_set:
+    if sb not in main_set:
+        print(sb)
+
+# 创建一个新的列表来存储复制后的数据
+new_rows = []
+
+# 复制并生成新的 test Parameters
+# for i in range(1, 201):  # 复制200次
+#     for row in rows:
+#         ordinal = get_ordinal_suffix(i)  # 根据复制次数生成序号后缀
+#         new_row = row.copy()  # 复制当前行，防止修改原始行
+#         new_row['TestParameters'] = f"{new_row['TestParameters']}_{ordinal}"  # 更新 test Parameters 列
+#         new_rows.append(new_row)  # 将新的行添加到新的列表中
+#         print(new_row)
+#
+# # 写入新的CSV文件
+# with open('updated_file.csv', mode='w', newline='', encoding='utf-8') as outfile:
+#     fieldnames = reader.fieldnames  # 获取原始CSV中的列名
+#     writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+#
+#     writer.writeheader()  # 写入列名
+#     writer.writerows(new_rows)  # 写入所有复制后的行
