855fe8979d054b6f644761b5da184d1202b4990a
Dorgon <dorgon.aisin@gmail.com>
2025-03-06 19:12:30 +0800

    add_Tech_Coverage: 新增添加require行功能

--- add_Tech_Coverage.py
+++ add_Tech_Coverage.py
@@ -1,5 +1,8 @@
 import os
 
+# 用于在action lua开头处写入行
+# 适用tag, require等
+
 def add_comment_to_action_lua(root_dir):
     for root, dirs, files in os.walk(root_dir):
         first_level_dir = os.path.basename(root)
@@ -15,10 +18,16 @@ def add_comment_to_action_lua(root_dir):
 
                     coverage = str(file).replace(".lua","")
                     # 构造注释行
-                    comment_line = f"-- {first_level_dir},{coverage}\n"
+                    # comment_line = f"-- {first_level_dir},{coverage}\n"
 
-                    with open(file_path, "w") as f:
-                        f.write(comment_line+content)
+                    # 构造require行
+                    comment_line = """local String = require("FXModules/FXLuaFunc/Common/String")\n"""
+
+                    #写入行
+                    if comment_line not in content:
+                        with open(file_path, "w") as f:
+                            f.write(comment_line+content)
+                        print(comment_line)
 
 root_dir = "/Users/device/Desktop/de_Aim_Bot/"
 add_comment_to_action_lua(root_dir)
